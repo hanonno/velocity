877a5736addbdc6d66474be90352703d5c1927d1
Hanno ten Hoor <hanonno@gmail.com>
2012-04-05 17:41:24 +0200

    Added sections & experiments

--- config/telegraaf.js
+++ config/telegraaf.js
@@ -8,22 +8,6 @@ module.exports = categories = [
             'http://www.telegraaf.nl/index.jsp?view=rss&showimage=true&imageversion=c&showfulltext=true',
         ]
     }, {
-        'name': 'telegraaf_business',
-        'display_name': 'de financiële telegraaf',
-        'display_name_short': 'DFT',        
-        'locale': 'nl',
-        'feeds': [
-            'http://www.telegraaf.nl/dft/index.jsp?view=rss&showimage=true&imageversion=c&showfulltext=true',
-        ]
-    }, {
-        'name': 'telegraaf_tech',
-        'display_name': 'digitaal',
-        'display_name_short': 'Digitaal',        
-        'locale': 'nl',
-        'feeds': [
-            'http://www.telegraaf.nl/digitaal/index.jsp?view=rss&showimage=true&imageversion=c&showfulltext=true',
-        ]
-    }, {
         'name': 'telegraaf_prive',
         'display_name': 'privé',
         'display_name_short': 'Privé',
@@ -38,6 +22,30 @@ module.exports = categories = [
         'locale': 'nl',
         'feeds': [
             'http://www.telegraaf.nl/telesport/index.jsp?view=rss&showimage=true&imageversion=c&showfulltext=true',
+        ]
+    }, {
+        'name': 'telegraaf_business',
+        'display_name': 'de financiële telegraaf',
+        'display_name_short': 'DFT',        
+        'locale': 'nl',
+        'feeds': [
+            'http://www.telegraaf.nl/dft/index.jsp?view=rss&showimage=true&imageversion=c&showfulltext=true',
+        ]
+    }, {
+        'name': 'telegraaf_cars',
+        'display_name': 'autovisie',
+        'display_name_short': 'Autovisie',
+        'locale': 'nl',
+        'feeds': [
+            'http://www.telegraaf.nl/autovisie/index.jsp?view=rss&showimage=true&imageversion=c&showfulltext=true'
+        ]
+    }, {
+        'name': 'telegraaf_tech',
+        'display_name': 'digitaal',
+        'display_name_short': 'Digitaal',        
+        'locale': 'nl',
+        'feeds': [
+            'http://www.telegraaf.nl/digitaal/index.jsp?view=rss&showimage=true&imageversion=c&showfulltext=true',
         ]
     }, {
         'name': 'telegraaf_woman',
