d78fb80f282f7dc703c379fb41467d6c7bdf6353
Hanno ten Hoor <hanonno@gmail.com>
2012-04-11 14:25:04 +0200

    Turn logging back on

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ Feedparser = require('feedparser')
 
 app.configure(function(){
 	app.use(express.static(__dirname + '/public'))
-/* 	app.use(express.logger()); */
+	app.use(express.logger());
 	app.use(express.bodyParser())
 	app.use(express.cookieParser())
 	app.use(app.router)
